3303fd4999e35a2aee490685d782a19d993dbf9c
Milky19 <97377003+Milky19@users.noreply.github.com>
2025-08-19 07:45:08 +0530

    Update Student.java

--- Student.java
+++ Student.java
@@ -9,6 +9,7 @@ public class Student {
         System.out.println("My Roll Number is: " + rollno);
         System.out.println("=======================");
          System.out.println("WELCOME TO JENKINS");
+        System.out.println("GOOD MORNING!!!!!!!");
         System.out.println("POLL SCM TASK ");
         System.out.println("WEBHOOK ");
     }
