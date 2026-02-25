ad17255703b080034860bbe2d1af14ad3797bfa9
challa-harini <challaharini6@gmail.com>
2026-02-25 12:05:57 +0530

    Update Student.java

--- Student.java
+++ Student.java
@@ -8,7 +8,7 @@ public class Student {
         System.out.println("My Course is: " + course);
         System.out.println("My Roll Number is: " + rollno);
         System.out.println("=======================");
-          System.out.println("Good Afternoon");
+          System.out.println("Welcome to Jenkins Triggers");
     }
 }
        
